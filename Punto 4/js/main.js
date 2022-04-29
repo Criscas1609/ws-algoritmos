c61509acf4fc01d9b84f58c470bf98b0bd6c7247
Cristhian C <ceballoscristhian4@gmail.com>
2022-04-29 05:50:42 -0500

    Adelanto del punto 4

--- Punto 4/js/main.js
+++ Punto 4/js/main.js
@@ -1,5 +1,8 @@
-"use strict";
+
 const descuento = document.getElementById("boton1")
-descuento.onclick=function(){
-    alert("Hola");
-};
+descuento.onclick=des
+
+let rebaja = Math.floor((Math.random() * 4) + 1)*10;
+function des(){
+    alert(` Sorpresa Tienes ${rebaja}% en tu compra`)
+}
