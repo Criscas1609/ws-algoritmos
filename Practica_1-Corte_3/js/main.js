9a2eb7d87738dccabb26fcb60970390cf63b967f
Cristhian C <ceballoscristhian4@gmail.com>
2022-05-16 23:18:47 -0500

    Adelanto

--- Practica_1-Corte_3/js/main.js
+++ Practica_1-Corte_3/js/main.js
@@ -1,1 +1,7 @@
-let
+function captura (){
+let producto = document.getElementById("nuevop").value
+let precio = document.getElementById("precio").value
+let cantidad = document.getElementById("cantidad").value
+let categoria = document.getElementsByClassName("categoria").value
+alert(`${producto}---$ ${precio}---${cantidad}----${categoria}`)
+}
