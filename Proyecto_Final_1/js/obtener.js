24aa830e1875b476b7b53f629e1dbed8100b10b7
Cristhian <ccorrea1068@cue.edu.co>
2022-06-17 11:22:36 -0500

    cambios proyectos

--- Proyecto_Final_1/js/obtener.js
+++ Proyecto_Final_1/js/obtener.js
@@ -1,52 +1,44 @@
-'use strict';
-//cart = carrito
-let trbody_html = document.querySelector(`.tbody`)
-let cart=[]
-    export default function addToObject(e){
-      const button_2 = e.target
-      //Obtiene toda la clase que contiene a al producto
-      let food_item = button_2.closest(`.card`);
-      let name_item = food_item.querySelector(`.card-title`).textContent;
-      let price_item = food_item.querySelector(`.precio`).textContent;
-      let photo_item = food_item.querySelector(`.card-img-top`).src;
-      const global_item = {
-        "title": name_item,
-        "price": price_item,
-        "photo": photo_item,
-        "stock": 1
-      }
-      addToCart(global_item)
+`use strict`;
+let carrito=[];
+let trbody_html=document.querySelector(`.tbody`);
+export default function añadir_objeto(e){
+    const button_2 = e.target
+    //Obtiene toda la clase que contiene a al producto
+    let contenedor_articulo = button_2.closest(`.card`);
+    let nombre_articulo = contenedor_articulo.querySelector(`.card-title`).textContent;
+    let imagen_articulo = contenedor_articulo.querySelector(`.card-img-top`).src;
+    let precio_articulo = contenedor_articulo.querySelector(`.precio`).textContent;
+    const objeto_articulo = {
+      "Nombre": nombre_articulo,
+      "Imagen": imagen_articulo,
+      "Precio": precio_articulo,
+      "Cantidad": 1
     }
-    function addToCart(global_item){
-        //const InputElemnto = tbody.getElementsByClassName('input__elemento');
-        for(let i =0; i < cart.length ; i++){
-          if(cart[i].title.trim() === global_item.title.trim()){
-            cart[i].stock ++;
-            //const inputValue = InputElemnto[i]
-            //inputValue.value++;
-            //CarritoTotal()
-            
-            return null;
-          }
-        }
-        cart.push(global_item)
-        console.log(cart)
-        show_in_carrito()
-    }
-    function show_in_carrito(){
+    añadir_al_carrito(objeto_articulo);
+  }
+    function añadir_al_carrito(objeto_articulo){
+      carrito = carrito.filter((valor, indice) => {
+        return carrito.indexOf(valor) === indice;
+      })
+      carrito.push(objeto_articulo);
+      console.log(carrito)
+    mostrar_html(carrito)
+  //Con esta funcion nos muestra el producto en el apartado de carrito
+    
+  function mostrar_html(carrito){
         trbody_html.innerHTML=``;
-        cart.map(item =>{
+        carrito.map(item =>{
             let new_tr= document.createElement(`tr`)
             new_tr.classList.add(`itemCart`)
             let content = `
             <th scope="row">1</th>
             <td class="table__productos">
-              <img src=${item.photo}  alt="">
-              <h6 class="title">${item.title}</h6>
+              <img src=${item.Imagen}  alt="">
+              <h6 class="title">${item.Nombre}</h6>
             </td>
-            <td class="table__price"><p>${item.price}</p></td>
+            <td class="table__price"><p>${item.Precio}</p></td>
             <td class="table__cantidad">
-              <input type="number" min="1" value=${item.stock} class="input__elemento">
+              <input type="number" min="1" value=${item.Cantidad} class="input__elemento">
               <button class="delete btn btn-danger">x</button>
             </td>
             `
@@ -54,4 +46,5 @@ let cart=[]
             trbody_html.appendChild(new_tr)
         })
     }
+}
     
